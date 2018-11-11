92721355569e4b74c43d3365ac9699096c30d4d4
Marin <wiellersenmarin@gmail.com>
2018-11-11 18:24:17 +0100

    home

--- front-page.php
+++ front-page.php
@@ -5,13 +5,65 @@
 	<div id="video-background" style="background-image:url(http://localhost/poolconsult/wp-content/uploads/2018/11/Pool-Consult-banner2-e1541790448532.jpg)">	
 	</div>
 	<div id='hometitles'>
-		<h1 class="title">Onafhankelijk advies bij bouw en renovatie van privé zwembaden</h1>
+		<h1 class="title">Pool Consult</h1>
+		<h2 class="title">Onafhankelijk advies en begeleiding bij bouw en renovatie van privé zwembaden</h2>
+		<a class="button-banner">Neem contact op ></a>
 	</div>
 </div>
 
 <div class='container-home'>
-<h1>Over Pool Consult</h1>
-<p>Pool Consult is een onafhankelijk en objectief adviesorgaan op het gebied van privé zwembaden met directe omgeving daarvan zoals whirlpool, sauna, tuin, terras, pool house en meer.</p> 
-</div>	
+	<div class='inleiding'>
+		<h1>Objectief en onafhankelijk</h1>
+		<span></span>
+		<p>Leveranciers en installateurs zijn niet onafhankelijk en zullen zelden een product aanbevelen dat beter is dan hun eigen programma. Pool Consult is daarentegen objectief en onafhankelijk. Wij adviseren en begeleiden particulieren en kleinschalige openbare instanties op het gebied van privé zwembaden met directe omgeving  daarvan, zoals een whirlpool, sauna, tuin, terras en pool house.</p> 
+		<a class="button-inleiding">Lees meer ></a>
+	</div>
+
+	<div class='diensten'>
+		<div class='flexbox'>
+			<div class='flexbox-flex'>
+			<h3>Nieuwbouw en renovatie</h3>
+			<span></span>
+			<p>Bij kleine projecten is vaak alleen een goed advies met een schetsontwerp voldoende, maar voor de complexe projecten is volledig bouwtraject vaak wenselijk.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+			<div class='flexbox-flex'>
+			<h3>Probleem ondersteuning</h3>
+			<span></span>
+			<p> In geval van een conflict tussen zwembadeigenaar en zwembadbouwer of een verzekeringsmaatschappij kunnen wij een onafhankelijk onderzoek doen.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+			<div class='flexbox-flex
+			'>
+			<h3>Deskundigen onderzoek</h3>
+			<span></span>
+			<p>Wij kunnen als onafhankelijk expert  onderzoek, analyse en rapportage uitvoeren wat als ondersteuning dient voor het besluit van de rechtbank bij problemen.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+		</div>
+		
+		<div class='flexbox'>
+			<div class='flexbox-flex'>
+			<h3>Taxaties</h3>
+			<span></span>
+			<p>In geval van schade kan Pool Consult behulpzaam zijn voor een verzekeringsmaatschappij.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+			<div class='flexbox-flex'>
+			<h3>Inspectie en keuring</h3>
+			<span></span>
+			<p>Pool Consult kan een onafhankelijke keuring doen van het zwembad en een inventarisatie van de exploitatie en eventuele mankementen en daaraan verbonden kosten.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+			<div class='flexbox-flex
+			'>
+			<h3>Energiebesparing</h3>
+			<span></span>
+			<p>Pool Consult kan een energiebesparingsonderzoek verrichten en een opgave doen voor verbetering met de terugverdientijd.</p> 
+			<a class="link-diensten">Lees meer ></a>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
